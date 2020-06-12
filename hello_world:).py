f3289827954c9247fb520bbe6ec08e536b515753
Phillip Tran <donutdeflector@tuta.io>
2020-06-12 00:54:04 -0400

    styled line 3 text

--- hello_world:).py
+++ hello_world:).py
@@ -1,6 +1,6 @@
 name = input("Enter your name: ")
 age = input("Enter your age: ")
-is_artsy = input ("Enter your opinion")
+is_artsy = input ("Enter your opinion: ")
 phillip = "dumb"
 
 if name.lower() == "phillip":
