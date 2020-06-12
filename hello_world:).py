bf0d414474e6dfbe37d68b1f4407d053a645199d
yc_pixel <Lilycheng2016@gmail.com>
2020-06-11 22:51:24 -0600

    Created 1st program; looks like Phillip didn't fail all the way

---
+++ hello_world:).py
@@ -0,0 +1,18 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+is_artsy = input ("Enter your opinion")
+phillip = "dumb"
+
+if name.lower() == "phillip":
+    print("ur dumb but i love u")
+elif name.upper() == "YC":
+    print("your life is meaningless")
+else:
+    print("you suck anyway")
+    if int(age) > 19:
+        print("you sus like phillip")
+
+print(f'name: {name}')
+print(f'age: {age}')
+print(f'is_artsy: {is_artsy}') 
+print (f'phillip:{phillip}')
